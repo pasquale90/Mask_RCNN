9716d9702c986bf363f4716b8f10cb6bf15cd3e9
pasquale90 <melissaspaschalis@gmail.com>
2022-03-14 13:46:09 +0200

    changed conda env to mask -  env

--- samples/insects/train_insects.sh
+++ samples/insects/train_insects.sh
@@ -2,6 +2,6 @@
 
 #initializing python
 source /home/melissap/miniconda3/etc/profile.d/conda.sh
-conda activate kumar
+conda activate mask
 
 python3 train_insects.py train --dataset=/data/CoRoSect/10.code/maskRCNN/Mask_RCNN_matterport/mask_rcnn/datasets/Nasekomo_insects --weights=/data/CoRoSect/10.code/maskRCNN/Mask_RCNN_matterport/mask_rcnn_balloon.h5
