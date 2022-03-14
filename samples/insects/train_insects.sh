368cfd8630a264d74db4ddef11128c7b2b42715c
pasquale90 <melissaspaschalis@gmail.com>
2022-03-14 13:56:53 +0200

    modified training scripts

--- samples/insects/train_insects.sh
+++ samples/insects/train_insects.sh
@@ -4,4 +4,4 @@
 source /home/melissap/miniconda3/etc/profile.d/conda.sh
 conda activate mask
 
-python3 train_insects.py train --dataset=/data/CoRoSect/10.code/maskRCNN/Mask_RCNN_matterport/datasets/Nasekomo_insects --weights=/data/CoRoSect/10.code/maskRCNN/Mask_RCNN_matterport/mask_rcnn_balloon.h5
+python3 train_insects.py train --dataset=datasets/Nasekomo_insects --weights=mask_rcnn_balloon.h5
